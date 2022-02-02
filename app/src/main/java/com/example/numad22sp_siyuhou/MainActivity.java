f4a213747ee9a5a1b1a5f1a54d29fc04d59bbf76
siyu3 <meikoshans@gmail.com>
2022-02-01 20:49:23 -0800

    add new button to open new page

--- app/src/main/java/com/example/numad22sp_siyuhou/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_siyuhou/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.numad22sp_siyuhou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     public void toastMsg(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
         toast.show();
@@ -21,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
